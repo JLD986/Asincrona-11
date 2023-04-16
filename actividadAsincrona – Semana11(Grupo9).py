7edd63435288fb7ee1cadb670b4971ef838127e9
JLD986 <126208860+JLD986@users.noreply.github.com>
2023-04-15 22:00:46 -0600

    Update actividadAsincrona – Semana11(Grupo9).py

--- actividadAsincrona – Semana11(Grupo9).py
+++ actividadAsincrona – Semana11(Grupo9).py
@@ -1,37 +1,46 @@
-print("*******************************************    Inicio del programa    *******************************************\n")
-vali= True
-while vali==True:
-    num= int(input("ingrese la cantidad de datos a procesar "))
+# Obtener la cantidad de números que se van a ingresar
+while True:
+    try:
+        n = int(input("""===========================================
+Ingrese la cantidad de números a clasificar
+============================================\n"""))
+        if n <= 0:
+            print("""===========================================
+Ingrese un número mayor a cero
+===========================================""")
+            continue
+        break
+    except ValueError:
+        print("""===========================================
+Ingrese un número válido
+===========================================\n""")
+
+# Inicializar variables para contar los números
+positivos = 0
+negativos = 0
+nulos = 0
+
+# Obtener los números y clasificarlos por su tipo
+for i in range(n):
+    while True:
+        try:
+            num = int(input(f"Ingrese el número {i+1}: "))
+            break
+        except ValueError:
+            print("Ingrese un número válido")
     if num > 0:
-        vali=False
-        posi=0
-        nega=0
-        nul=0
-
-        for i in range(num):
-            valor = input("Ingrese un valor: ")
-            if valor.isdigit():
-                data=int(valor)
-                if data > 0:
-                    posi+=1
-                elif data < 0:
-                    nega+=1
-                else:
-                    nul+=1
-            elif valor.isalpha():
-                print("a ingresado una palabra ")
-                for i in valor:
-                    print(i)
-            else:
-                print("Probablemente esto es un signo")
-        print(f"\nLa cantidad de numeros positivos es {posi}")
-        print(f"\nLa cantidad de numeros negativos es {nega}")
-        print(f"\nLa cantidad de numeros nulos es {nul}")
-
+        positivos += 1
+    elif num < 0:
+        negativos += 1
     else:
-        print("\nEl numero ingresado no es correcto intentalo nuevamente")
+        nulos += 1
 
+# Mostrar los resultados obtenidos que ingreso el usuario
+print(f"La cantidad de números positivos es: {positivos}")
+print(f"La cantidad de números negativos es: {negativos}")
+print(f"La cantidad de números nulos es: {nulos}")
 
-print(print("*******************************************    Fin del programa    *******************************************\n"))
+# Mensaje donde indica la finalizaciond el programa
+print("Programa finalizado")
 
 
